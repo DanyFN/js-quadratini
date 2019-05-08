3c847a5935d11f8a43be0da683ad2a5c45d62e84
DanyFN <danielefontana90@gmail.com>
2019-05-09 00:15:54 +0200

    campo minato fix

--- main.js
+++ main.js
@@ -6,17 +6,3 @@ $('.mina').click(function(){
 $('.quadrato').click(function(){
   $(this).addClass('green')
 })
-
-$(document).ready(function(){
-
-
-var quadrato = $('.quadrato');
-
-var griglia = quadrato *64;
-
-console.log(griglia);
-
-
-
-
-});
